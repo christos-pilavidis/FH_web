10a1163ad307fa0f48db4f4f8bd40f959a402077
Christos Pilavidis <chris.pilavidis@hotmail.com>
2024-11-21 17:41:05 +0200

    Update index.ts

--- data/layout/header/v1/index.ts
+++ data/layout/header/v1/index.ts
@@ -3,8 +3,4 @@ import { menuItemsProps } from '../menu';
 
 export const headerData: HeaderProps = {
   menuItems: menuItemsProps,
-  contactInfo: {
-    title: 'Need help?',
-    email: 'info@foxhoundgames.com', // Replace with your actual email
-  },
 };
